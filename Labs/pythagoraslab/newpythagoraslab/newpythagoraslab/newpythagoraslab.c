2617c8e94fca5776479946c81cccf8d02ad31aa9
tianna vollrath <tianna_vollrath@yahoo.com>
2019-09-18 08:53:39 -0700

    i was here

---
+++ Labs/pythagoraslab/newpythagoraslab/newpythagoraslab/newpythagoraslab.c
@@ -0,0 +1,9 @@
+//
+//  newpythagoraslab.c
+//  newpythagoraslab
+//
+//  Created by Vollrath, Tianna on 9/18/19.
+//  Copyright © 2019 Vollrath, Tianna. All rights reserved.
+//
+
+#include "newpythagoraslab.h"
