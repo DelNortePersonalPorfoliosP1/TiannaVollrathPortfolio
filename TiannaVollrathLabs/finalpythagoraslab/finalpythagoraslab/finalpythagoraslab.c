4c348016ae96173b94e1227be50c7d2a909a152e
John Mortensen <jmortensen@powayusd.com>
2019-09-19 09:30:35 -0700

    start

--- TiannaVollrathLabs/finalpythagoraslab/finalpythagoraslab/finalpythagoraslab.c
+++
@@ -1,9 +0,0 @@
-//
-//  finalpythagoraslab.c
-//  finalpythagoraslab
-//
-//  Created by Vollrath, Tianna on 9/18/19.
-//  Copyright © 2019 Vollrath, Tianna. All rights reserved.
-//
-
-#include "finalpythagoraslab.h"
--- TiannaVollrathLabs/newpythagoraslab/newpythagoraslab/newpythagoraslab.c
+++
@@ -1,9 +0,0 @@
-//
-//  newpythagoraslab.c
-//  newpythagoraslab
-//
-//  Created by Vollrath, Tianna on 9/18/19.
-//  Copyright © 2019 Vollrath, Tianna. All rights reserved.
-//
-
-#include "newpythagoraslab.h"
