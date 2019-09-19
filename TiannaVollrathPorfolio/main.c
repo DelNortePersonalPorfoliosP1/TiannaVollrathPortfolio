692a671b23408a0a61930cbd4954886460232442
John Mortensen <jmortensen@powayusd.com>
2019-09-19 09:42:08 -0700

    update repo

---
+++ TiannaVollrathPorfolio/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  TiannaVollrathPorfolio
+//
+//  Created by John Mortensen on 9/19/19.
+//  Copyright © 2019 Vollrath, Tianna. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "pythagoras.h"
+
+int main()
+{
+    pythagoras();
+    return 0;
+}
